2ff1e96943371a0678961b7fa265cd6ff348f0b4
Paco-Pastor <pacopas1512@gmail.com>
2023-12-27 12:30:05 +0100

    💩 Attempt to run docker exec w/ node container

--- services/ihm/back/index.js
+++ services/ihm/back/index.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require("path");
+const { exec } = require("child_process");
 
 const app = express();
 const port = 3000;
 
-// Définit le dossier statique pour le front
-app.use(express.static('front'));
+app.use(bodyParser.json());
 
-// Route pour la page principale
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../front/index.html'));
+// Définit le dossier statique pour le front
+app.use(express.static("front"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../front/index.html"));
 });
 
-// Écoute sur le port spécifié
+app.post("/io", (req, res) => {
+
+  const file = req.body.file;
+  const dockerExecCommand = `docker exec avg-grade-ultimate-io-1 dotnet Io.dll ${file}`;
+  // BUG : docker n'existe pas dans ce contexte.
+
+  exec(dockerExecCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(
+        `Erreur lors de l'exécution de la commande Docker exec: ${stderr}`
+      );
+      res
+        .status(500)
+        .send("Erreur lors de l'exécution de la commande Docker exec");
+    } else {
+      console.log(`Résultat de la commande Docker exec: ${stdout}`);
+      res.status(200).send("Commande Docker exec exécutée avec succès");
+    }
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Le serveur est lancé sur http://localhost:${port} my G`);
+  console.log(`Le serveur est lancé sur http://localhost:${port}`);
 });
