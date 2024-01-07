12886667e67e32428e94ab47504b0c06315d7b95
Paco-Pastor <pacopas1512@gmail.com>
2024-01-07 15:33:03 +0100

    ✨ Update IHM /io route to call io service

--- services/ihm/back/index.js
+++ services/ihm/back/index.js
@@ -1,39 +1,26 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
-const { exec } = require("child_process");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-
-// Définit le dossier statique pour le front
 app.use(express.static("front"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/../front/index.html"));
 });
 
-app.post("/io", (req, res) => {
-
-  const file = req.body.file;
-  const dockerExecCommand = `docker exec avg-grade-ultimate-io-1 dotnet Io.dll ${file}`;
-  // BUG : docker n'existe pas dans ce contexte.
-
-  exec(dockerExecCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(
-        `Erreur lors de l'exécution de la commande Docker exec: ${stderr}`
-      );
-      res
-        .status(500)
-        .send("Erreur lors de l'exécution de la commande Docker exec");
-    } else {
-      console.log(`Résultat de la commande Docker exec: ${stdout}`);
-      res.status(200).send("Commande Docker exec exécutée avec succès");
-    }
-  });
+app.post("/io", async (req, res) => {
+  try {
+    const response = await axios.post("http://localhost:5000/io", req.body);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
